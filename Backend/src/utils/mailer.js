3e649e890cd335877280cc0b92c5178a11dd1c69
RaulDiaz07 <pedrotijolarias@gmail.com>
2026-05-11 10:11:59 -0300

    deu certo recuperação de senha

--- Backend/src/utils/mailer.js
+++ Backend/src/utils/mailer.js
@@ -1,11 +1,13 @@
 const { Resend } = require("resend");
+const EmailDominio = "onboarding@resend.dev"
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const enviarEmailVerificacao = async (email, token) => {
   const link = `${process.env.BASE_URL}/api/auth/verificar-email?token=${token}`;
+  
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: EmailDominio,
     to: email,
     subject: "Verifique seu email",
     html: `
@@ -17,4 +19,42 @@ const enviarEmailVerificacao = async (email, token) => {
   });
 };
 
-module.exports = { enviarEmailVerificacao };
+
+
+const enviarEmailRecuperacao =
+  async (email, token) => {
+
+    const link =
+      `http://localhost:5173/redefinir-senha?token=${token}`;
+
+    await resend.emails.send({
+      from: EmailDominio,
+
+      to: email,
+
+      subject: "Recuperação de senha",
+
+      html: `
+        <h2>Recuperação de senha</h2>
+
+        <p>
+          Clique no botão abaixo
+          para redefinir sua senha:
+        </p>
+
+        <a href="${link}">
+          Redefinir senha
+        </a>
+
+        <p>
+          Este link expira em 1 hora.
+        </p>
+      `,
+    });
+};
+
+module.exports = {
+  
+  enviarEmailVerificacao,
+  enviarEmailRecuperacao
+};
